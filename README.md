17ffdb8f6fe03948ebcd50078e84f74c56e6d3f4
ben <3290701522@qq.com>
2023-08-11 19:10:47 +0800

    add dijkstra

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 | 序号 | 文件 | 视频内容 |
 | --- | --- | --- |
-| 1 | [dijkstra\dijkstra_plain.py](dijkstra/dijkstra_plain.py) | 【简单算法】最短路径——纯手动迪节斯特拉算法 |
+| 1 | [dijkstra/dijkstra_plain.py](dijkstra/dijkstra_plain.py) | 【简单算法】最短路径——纯手动迪节斯特拉算法 |
--- dijkstra/dijkstra_plain.py
+++ dijkstra/dijkstra_plain.py
@@ -1,11 +1,18 @@
+"""
+dijkstra算法朴素实现
+"""
+
+
 def get_next(nodes_to_visit):
+    """从 P 中选出下一个要访问的点"""
     node, _ = min(nodes_to_visit.items(), key=lambda x: x[1][0])
     return node
 
 def dijkstra(graph, source, dest):
+    # nodes_to_visit -> P
     nodes_to_visit = {node: (float('inf'), None) for node in graph}
     nodes_to_visit[source] = (0, None)
-
+    # 访问过的点会被丢到这里
     nodes_visited = {}
 
     while nodes_to_visit:
@@ -33,16 +40,16 @@ def dijkstra(graph, source, dest):
 if __name__ == "__main__":
     # Sample weighted graph represented as an adjacency dictionary
     graph = {
-        'A': {'C': 3, 'F': 2},
-        'C': {'A': 3, 'F': 2, 'D': 4, 'E':1},
-        'F': {'A': 2, 'C': 2, 'E': 3, 'B': 6},
-        'E': {'C': 1, 'F': 3, 'B': 2},
-        'D': {'B': 1, 'C': 4},
-        'B': {'D': 1, 'E': 2, 'F': 6}
+        'S': {'C': 3, 'F': 2},
+        'C': {'S': 3, 'F': 2, 'D': 4, 'E':1},
+        'F': {'S': 2, 'C': 2, 'E': 3, 'T': 6},
+        'E': {'C': 1, 'F': 3, 'T': 2},
+        'D': {'T': 1, 'C': 4},
+        'T': {'D': 1, 'E': 2, 'F': 6}
     }
 
-    source_node = 'A'
-    dest_node = 'E'
+    source_node = 'S'
+    dest_node = 'T'
     shortest_distance, path = dijkstra(graph, source_node, dest_node)
 
     print("Shortest distances from node {} to {}: {}".format(source_node, dest_node, shortest_distance))
